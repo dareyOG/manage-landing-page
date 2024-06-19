9137d5f7b8a0816e1dcf33f4576f118ffa42fea7
Oludire Damilare <oludire@gmail.com>
2024-06-19 18:57:17 +0100

    Mobile view 70% done

---
+++ src/testimonials.js
@@ -0,0 +1,26 @@
+export const testimonials = [
+  {
+    author: 'Anisha Li',
+    avatar: './images/avatar-anisha.png',
+    testimonial:
+      'Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.',
+  },
+  {
+    author: 'Ali Bravo',
+    avatar: './images/avatar-ali.png',
+    testimonial:
+      'We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.',
+  },
+  {
+    author: 'Richard Watts',
+    avatar: './images/avatar-richard.png',
+    testimonial:
+      'Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.',
+  },
+  {
+    author: 'Shanai Gough',
+    avatar: './images/avatar-shanai.png',
+    testimonial:
+      'Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.',
+  },
+];
